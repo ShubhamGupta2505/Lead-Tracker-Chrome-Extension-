88ef02d110197ca20dc74069f65c9f073b820cac
ShubhamGupta2505 <shubham3nemo@gmail.com>
2022-08-05 11:48:38 +0530

    Function parameters

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const inputbtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deletebtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
 function render(leads) {
     let listItems = ""
     for(let i =0;i<leads.length;i++) {
@@ -23,30 +25,22 @@ function render(leads) {
 }
     ulEl.innerHTML = listItems
 }
+
 deletebtn.addEventListener("dblclick",function() {
-    // console.log("Double clicked!!")
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
-
-// console.log(leadsFromLocalStorage)
 inputbtn.addEventListener("click",function() {
-    // console.log("Button clicked from addEventListener")
     myLeads.push(inputEL.value)
     inputEL.value = ""
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
     render(myLeads)
-    // console.log(localStorage.getItem("myLeads"))
 })
 
-// const recipient = "James"
-// const sender = "Shubham"
-// const email = `
-// Hey ${recipient}!
-// How is it going?
-// Cheers ${sender}
-// `
-// console.log(email)
-
+const welcomeEl = document.getElementById("welcome-el")
+function greetUser(greeting,name,emoji) {
+    welcomeEl.textContent =  `${greeting}  ,  ${name}  ${emoji}`
+}
+greetUser("Howdy","james","!")
